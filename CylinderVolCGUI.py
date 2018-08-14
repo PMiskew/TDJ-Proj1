c234059cca7062bd37859c1b47a404dba3dd4bae
PMiskew <paul.miskew@gmail.com>
2018-08-14 09:59:11 -0400

    Class Program

---
+++ CylinderVolCGUI.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import math
+
+class CylinderCalc:
+
+	def __init__(self):
+
+		print("Creating Cylinder Calculator Object")
+
+		self.root = tk.Tk()
+		self.root.title("Cylinder Calculator")
+
+		self.root.mainloop();
+
+
+cylinderCalc = CylinderCalc();
